4572532944bec871424e3bf0bcd10a6c7d4fce07
primadita <pulomas10@hotmail.com>
2025-07-21 01:14:03 +0200

    feat: add load more button (without function)

--- templates.js
+++ templates.js
@@ -4,9 +4,9 @@ function getOverlayTemplate(array, id){
             <p id="poke-id${id}">#id</p>
             <h2 id="pokemon-name${id}">${array.results[id].name}</h2>
             <div>
-                <div id="types${id}">
-                    <div id="type1" class="ability">ability1</div>
-                    <div id="type2" class="ability">ability2</div>
+                <div id="types${id}" class="types">
+                    <div id="type1" class="type">ability1</div>
+                    <div id="type2" class="type">ability2</div>
                 </div>
                 <img id="overlay-img" src="./assets/icons/pokeball.png" alt="placeholder">
             </div>
