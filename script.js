d342a36de2954b5172e419758aa1c6e77b8adfcd
primadita <pulomas10@hotmail.com>
2025-07-20 14:20:05 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Spinner anzeigen
+function showSpinner() {
+  document.getElementById("spinner").style.display = "block";
+}
+
+// Spinner ausblenden
+function hideSpinner() {
+  document.getElementById("spinner").style.display = "none";
+}
+
+// to do: function for spinner
